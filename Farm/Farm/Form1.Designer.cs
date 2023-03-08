458a670577d982687cb216da4f3bc39fd780b127
Victor Henrique <tipovito41@gmail.com>
2023-03-08 11:39:16 -0300

    CS_rev_A

---
+++ Farm/Farm/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Farm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.farmDataSet = new Farm.farmDataSet();
+            this.materiaisBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.materiaisTableAdapter = new Farm.farmDataSetTableAdapters.materiaisTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.farmDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.materiaisBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Profissional";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(97, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Dia da Semana";
+            // 
+            // textBox1
+            // 
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.materiaisBindingSource, "mat_NM", true));
+            this.textBox1.Location = new System.Drawing.Point(195, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.materiaisBindingSource, "mat_Prof", true));
+            this.textBox2.Location = new System.Drawing.Point(195, 148);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.materiaisBindingSource, "mat_DS", true));
+            this.textBox3.Location = new System.Drawing.Point(195, 211);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // farmDataSet
+            // 
+            this.farmDataSet.DataSetName = "farmDataSet";
+            this.farmDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // materiaisBindingSource
+            // 
+            this.materiaisBindingSource.DataMember = "materiais";
+            this.materiaisBindingSource.DataSource = this.farmDataSet;
+            // 
+            // materiaisTableAdapter
+            // 
+            this.materiaisTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 357);
+            this.ControlBox = false;
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro de Materiais";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.farmDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.materiaisBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private farmDataSet farmDataSet;
+        private System.Windows.Forms.BindingSource materiaisBindingSource;
+        private farmDataSetTableAdapters.materiaisTableAdapter materiaisTableAdapter;
+    }
+}
+
---
+++ Farm/Farm/Form2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Farm
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void ClearAllBoxes()
+        {
+            Action<Control.ControlCollection> func = null;
+            func = (controls) =>
+            {
+                foreach (Control control in controls)
+                {
+
+                    if (control is TextBox || control is ComboBox)
+                    {
+                        control.ResetText();
+                    }
+                    else
+                        func(control.Controls);
+                }
+            };
+            func(Controls);
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // TODO: esta linha de código carrega dados na tabela 'farmDataSet.funcionarios'. Você pode movê-la ou removê-la conforme necessário.
+            this.funcionariosTableAdapter.Fill(this.farmDataSet.funcionarios);
+            // TODO: esta linha de código carrega dados na tabela 'farmDataSet.funcionarios'. Você pode movê-la ou removê-la conforme necessário.
+            this.funcionariosTableAdapter.Fill(this.farmDataSet.funcionarios);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ClearAllBoxes();
+        }
+    }
+}
